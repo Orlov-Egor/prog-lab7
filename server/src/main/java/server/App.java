a27634e25e10a903e53b20618061356a5c7755fa
slamach <drslampro@gmail.com>
2020-05-06 01:11:05 +0700

    Server port setup using cmd added

--- server/src/main/java/server/App.java
+++ server/src/main/java/server/App.java
@@ -1,5 +1,8 @@
 package server;
 
+import common.exceptions.NotInDeclaredLimitsException;
+import common.exceptions.WrongAmountOfElementsException;
+import common.utility.Outputer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import server.commands.*;
@@ -12,11 +15,37 @@ import server.utility.CommandManager;
  * @author Sviridov Dmitry and Orlov Egor.
  */
 public class App {
-    public static final int PORT = 1821;
     public static final String ENV_VARIABLE = "LABA";
     public static Logger logger = LogManager.getLogger("ServerLogger");
 
+    private static int port;
+
+    private static boolean initializePort(String[] portArgs) {
+        try {
+            if (portArgs.length != 1) throw new WrongAmountOfElementsException();
+            port = Integer.parseInt(portArgs[0]);
+            if (port < 0) throw new NotInDeclaredLimitsException();
+            return true;
+        } catch (WrongAmountOfElementsException exception) {
+            String jarName = new java.io.File(App.class.getProtectionDomain()
+                    .getCodeSource()
+                    .getLocation()
+                    .getPath())
+                    .getName();
+            Outputer.println("Использование: 'java -jar " + jarName + " <host> <port>'");
+        } catch (NumberFormatException exception) {
+            Outputer.printerror("Порт должен быть представлен числом!");
+            App.logger.fatal("Порт должен быть представлен числом!");
+        } catch (NotInDeclaredLimitsException exception) {
+            Outputer.printerror("Порт не может быть отрицательным!");
+            App.logger.fatal("Порт не может быть отрицательным!");
+        }
+        App.logger.fatal("Ошибка инициализации порта запуска!");
+        return false;
+    }
+
     public static void main(String[] args) {
+        if (!initializePort(args)) return;
         CollectionFileManager collectionFileManager = new CollectionFileManager(ENV_VARIABLE);
         CollectionManager collectionManager = new CollectionManager(collectionFileManager);
         CommandManager commandManager = new CommandManager(
@@ -38,7 +67,7 @@ public class App {
                 new FilterByWeaponTypeCommand(collectionManager),
                 new ServerExitCommand()
         );
-        Server server = new Server(PORT, commandManager);
+        Server server = new Server(port, commandManager);
         server.run();
     }
 }
